701e0c58b0a9f3fb367ff9b7b08311877500ccdc
Fuad Adhim Al Hasan <fuad@ordent.co.id>
2026-06-07 11:13:13 +0700

    feat: add favicon and standardize app title across layout files

--- resources/views/layouts/guardian.blade.php
+++ resources/views/layouts/guardian.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'SIM-SANTRI') }} - Wali Santri</title>
+    <link rel="shortcut icon" href="{{ asset('image/pondok.png') }}" type="image/png" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
